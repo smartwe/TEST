7f84a641ce17bbb224cb380b96e55b2a6e671d8d
smartwe <githubdev.smartwe@gmail.com>
2021-12-11 23:30:13 +0900

    .

--- main.js
+++ main.js
@@ -1,61 +1,52 @@
-const { autoUpdater } = require("electron-updater");
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
 
-let updateWin;
+let mainWindow;
 
-function sendStatusToWindow(text) {
-  updateWin.webContents.send("message", text);
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
 }
 
-function createDefaultUpdaetWindow() {
-  updateWin = new BrowserWindow({
-    backgroundColor: "#eeeeee",
-    webPreferences: { nodeIntegration: true },
-  });
+app.on('ready', () => {
+  createWindow();
+  autoUpdater.checkForUpdatesAndNotify();
+});
 
-  updateWin.on("closed", () => {
-    updateWin = null;
-  });
-  updateWin.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
-  return updateWin;
-}
-
-autoUpdater.on("checking-for-update", () => {
-  sendStatusToWindow("Checking for update...");
-});
-autoUpdater.on("update-available", (info) => {
-  sendStatusToWindow("Update available.");
-});
-autoUpdater.on("update-not-available", (info) => {
-  sendStatusToWindow("Update not available.");
-});
-autoUpdater.on("error", (err) => {
-  sendStatusToWindow("Error in auto-updater. " + err);
-});
-autoUpdater.on("download-progress", (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + " - Downloaded " + progressObj.percent + "%";
-  log_message = log_message + " (" + progressObj.transferred + "/" + progressObj.total + ")";
-  sendStatusToWindow(log_message);
-});
-autoUpdater.on("update-downloaded", (info) => {
-  sendStatusToWindow("Update downloaded");
-
-  const option = {
-    type: "question",
-    buttons: ["업데이트", "취소"],
-    defaultId: 0,
-    title: "electron-updater",
-    message: "업데이트가 있습니다. 프로그램을 업데이트 하시겠습니까?",
-  };
-  let btnIndex = dialog.showMessageBoxSync(updateWin, option);
-
-  if(btnIndex === 0) {
-    autoUpdater.quitAndInstall();
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
   }
 });
 
-app.on("ready", async () => {
-  createDefaultUpdaetWindow();
-  autoUpdater.checkForUpdates();
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+autoUpdater.on('update-available', () => {
+    mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+    mainWindow.webContents.send('update_downloaded');
+});
+
+ipcMain.on('restart_app', () => {
+    autoUpdater.quitAndInstall();
 });
